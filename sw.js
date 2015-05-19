48d80616d432b102d0d707f47898be09cf057aef
Anant Gupta <anantgupta@google.com>
2015-05-19 13:30:46 -0700

    @

--- sw.js
+++ sw.js
@@ -19,19 +19,7 @@ this.addEventListener('install', function(event) {
 console.log('See');
 this.addEventListener('fetch', function(event) {
   console.log('Got here!');
-  var response;
-  var cachedResponse = caches.match(event.request).then(function() {
-    console.log('Succeeded?');
-  }, function() {
-    console.log('Failed');
-    return fetch(event.request);
-  }).then(function(r) {
-    response = r;
-    caches.open('v1').then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }).catch(function() {
+
     return caches.match('/ServiceWorker/gallery/myLittleVader.jpg');
-  });
+
 });
