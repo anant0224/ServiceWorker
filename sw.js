4e8f31340cbd58aead08777c0ef7121c9ac2bd40
Anant Gupta <anantgupta@google.com>
2015-05-19 15:49:25 -0700

    asd

--- sw.js
+++ sw.js
@@ -1,20 +1,5 @@
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll(
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
-      );
-    })
-  );
+  console.log("Installed");
 });
 console.log('See');
 
@@ -24,7 +9,6 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   console.log('Got here!');
-  event.respondWidth(
-    caches.match('/ServiceWorker/gallery/myLittleVader.jpg'));
+  event.respondWith(new Response("Hello world!"));
 
 });
