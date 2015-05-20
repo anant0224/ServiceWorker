50597fec31ff78749762d7108a0d3e2512bb47c7
Anant Gupta <anantgupta@google.com>
2015-05-19 17:35:12 -0700

    asd

--- app.js
+++ app.js
@@ -15,28 +15,22 @@ if ('serviceWorker' in navigator) {
 function imgLoad(imgJSON) {
   // return a promise for an image loading
   return new Promise(function(resolve, reject) {
-    var request = new XMLHttpRequest();
-    request.open('GET', imgJSON.url);
-    request.responseType = 'blob';
 
-    request.onload = function() {
-      if (request.status == 200) {
+    var init = { method: 'GET' };
+    
+    fetch(imgJSON.url).then(function(response) {
+      if (response.status == 200) {
         var arrayResponse = [];
-        arrayResponse[0] = request.response;
+        arrayResponse[0] = response.blob();
         arrayResponse[1] = imgJSON;
         resolve(arrayResponse);
       } else {
-        reject(Error('Image didn\'t load successfully; error code:' + request.statusText));
+        reject(Error('Image didn\'t load successfully; error code:' + response.statusText));
       }
-    };
-
-    request.onerror = function() {
+    }, function() {
       reject(Error('There was a network error.'));
-    };
+    });
 
-    // Send the request
-    alert('hi');
-    request.send();
   });
 };
 
