949d74cbba031dba6d08c2966e3b757dde9426bf
Anant Gupta <anantgupta@google.com>
2015-05-19 18:14:59 -0700

    asd

--- image-list.js
+++ image-list.js
@@ -5,7 +5,7 @@ var Gallery = { 'images' : [
   {
     'name'  : 'Darth Vader',
     'alt' : 'A Black Clad warrior lego toy',
-    'url': 'index.html',
+    'url': 'gallery/myLittleVader.jpg',
     'credit': '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
   },
 
