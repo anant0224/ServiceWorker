5cd49e751ea816f313afebbefaf635a15176a2a2
Anant Gupta <anantgupta@google.com>
2015-05-21 16:21:48 -0700

    awd

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
-self.addEventListener('install', function(event) {
-  console.log("Installed");
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(
+        '/sw-test/',
+        '/sw-test/index.html',
+        '/sw-test/style.css',
+        '/sw-test/app.js',
+        '/sw-test/image-list.js',
+        '/sw-test/star-wars-logo.jpg',
+        '/sw-test/gallery/',
+        '/sw-test/gallery/bountyHunters.jpg',
+        '/sw-test/gallery/myLittleVader.jpg',
+        '/sw-test/gallery/snowTroopers.jpg'
+      );
+    })
+  );
 });
 console.log('See');
 
