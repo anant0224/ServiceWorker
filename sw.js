038cf10933ec778501348dffe8d71c3d7c061259
Anant Gupta <anantgupta@google.com>
2015-05-20 17:04:44 -0700

    asd

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-
+  console.log("Caught!");
 	// Clone the request for fetch and cache
 	// A request is a stream and can be consumed only once.
 	var fetchRequest = event.request.clone(),
