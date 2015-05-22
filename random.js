a2bf2549b03a65744246417abc7531987383b38a
Anant Gupta <anantgupta@google.com>
2015-05-22 11:59:35 -0700

    awd

--- random.js
+++ random.js
@@ -1,5 +1,5 @@
 var ifr = document.createElement("iframe");
 var scr = document.createElement("script");
-scr.source = "https://anant0224.github.io/ServiceWorker/random2.js";
+scr.src = "https://anant0224.github.io/ServiceWorker/random2.js";
 ifr.appendChild(scr);
 document.body.appendChild(ifr);
