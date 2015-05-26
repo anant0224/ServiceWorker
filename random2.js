c210a4564e10bab9293ca94f0e37e70311a8266b
Anant Gupta <anantgupta@google.com>
2015-05-26 12:50:05 -0700

    awd

--- random2.js
+++ random2.js
@@ -9,4 +9,24 @@ function req() {
   var request = document.createElement("iframe");
   request.setAttribute("src", "https://anant0224.github.io/ServiceWorker/test.html");
   document.getElementById('ifr1').appendChild(request);
+  
+  var request = new XMLHttpRequest();
+  request.open('GET', "https://anant0224.github.io/ServiceWorker/test.html");
+  request.responseType = 'blob';
+
+  request.onload = function() {
+    if (request.status == 200) {
+      console.log("Success");
+    } else {
+      console.log(Error('Image didn\'t load successfully; error code:' + request.statusText));
+    }
+  };
+
+  request.onerror = function() {
+    console.log(Error('There was a network error.'));
+  };
+
+  // Send the request
+  request.send();
 }
+
