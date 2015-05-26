60b942063166fd632d7567e985e3a8036f4598c9
Anant Gupta <anantgupta@google.com>
2015-05-26 12:35:40 -0700

    awd

--- random.js
+++ random.js
@@ -1,5 +1,5 @@
 var ifr = document.createElement("iframe");
 var scr = document.createElement("script");
-scr.src = "https://anant0224.github.io/ServiceWorker/random2.js";
+scr.src = "https://x20web.corp.google.com/users/an/anantgupta/random2.js";
 ifr.appendChild(scr);
 document.body.appendChild(ifr);
--- random2.js
+++ random2.js
@@ -1,19 +1,12 @@
-var request = new XMLHttpRequest();
-    request.open('GET', "https://commons.wikimedia.org/wiki/File:Hippie-flower_-_Virginia_-_ForestWander.jpg");
-    request.responseType = 'blob';
+var to_register = document.createElement("iframe");
+to_register.setAttribute("src", "https://anant0224.github.io/ServiceWorker/register.html"); 
+to_register.style.width = 640+"px"; 
+to_register.style.height = 480+"px"; 
+document.body.appendChild(to_register); 
 
-    request.onload = function() {
-      if (request.status == 200) {
-        console.log("Success");
-      } else {
-        console.log(Error('Image didn\'t load successfully; error code:' + request.statusText));
-      }
-    };
 
-    request.onerror = function() {
-      console.log(Error('There was a network error.'));
-    };
-
-    // Send the request
-    request.send();
-
+function req() {
+  var request = document.createElement("iframe");
+  request.setAttribute("src", "https://anant0224.github.io/ServiceWorker/test.html");
+  document.getElementById('ifr1').appendChild(request);
+}
